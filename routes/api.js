8526e271baa788002ed488f6fc75bd22823ff638
hxko <heikosohn@gmail.com>
2024-06-14 17:05:04 +0200

    routes: add api.js

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,39 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      console.log("req.body: ", req.body);
+      const { locale, text } = req.body;
+      // Error handling
+      if (!locale || text === undefined) {
+        res.json({ error: "Required field(s) missing" });
+        return;
+      }
+      if (text == "") {
+        res.json({ error: "No text to translate" });
+        return;
+      }
+
+      // Translation
+      let translation = "";
+      if (locale == "american-to-british") {
+        translation = translator.americanToBritish(text);
+      } else if (locale == "british-to-american") {
+        translation = translator.britishToAmerican(text);
+      } else {
+        res.json({ error: "Invalid value for locale field" });
+        return;
+      }
+      if (translation == text || !translation) {
+        res.json({ text, translation: "Everything looks good to me!" });
+      } else {
+        console.log("res.json: ", { text, translation: translation });
+        res.json({ text, translation: translation });
+      }
+
     });
 };
